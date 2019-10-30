604090ef472cb8aa0ce888bf7bfa4cbb792fca8a
Brad Fontaine <bradf.83@gmail.com>
2019-10-29 18:10:16 -0600

    Now able to create owners and delete owners.

--- src/routes/owner.js
+++ src/routes/owner.js
@@ -13,4 +13,21 @@ router.get('/:id', async (req, res) => {
     return res.status(200).json(owner);
 });
 
+router.post('/', async (req, res) => {
+    // TODO
+    // Create an owner from the request body, return created.
+    // What if there is an error?
+    // What if validations fail?
+    const created = await req.context.db.Owner.create(req.body);
+    return res.status(201).json();
+});
+
+router.delete('/:id', async (req, res) => {
+    // TODO
+    // Delete with a where clause
+    // This is async so the server will respond before finishing?
+    req.context.db.Owner.destroy({where: {id: req.params.id}});
+    return res.status(204).json();
+});
+
 export default router;
